ae296b19d5b7a87c4de27877f30dd6d196d07420
cuteJJong <whdgh9588@gmail.com>
2024-07-18 13:53:23 +0900

    07 17 자바소스

--- 0718/src/LambdaDemo.java
+++ 0718/src/LambdaDemo.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class LambdaDemo {
+/*
+* Lambda & Stream API
+* 1.Lambda
+*   1)원래 객체지향언어에서 사용하지 않고, Scala 나 Groovy 같은 함수형 언어에서 사용하던 문법
+*   2) 메소드는 클래스안에서 객체의 행위를 정의하지만, Expression이라고 쓴 이유는 Method의 역할과 동일하지만 특정
+*       클래스 소속이 아니기 때문이다.
+*
+* */
+public class LambdaDemo {
 }
--- ProductManagement/src/com/example/Output.java
+++ ProductManagement/src/com/example/Output.java
@@ -1,2 +1,23 @@
-package com.example;public class Output {
+package com.example;
+
+import java.util.List;
+
+public class Output {
+    private List<Product> list;
+    public Output(List<Product> list) {
+        this.list = list;
+    }
+
+    public void print() {
+        this.labelPrint();      //
+        for (Product p : this.list) {
+            System.out.println(p);      //p.toString() 호출
+        }
+    }
+    private void labelPrint() {
+        System.out.println("                       <<제품관리프로그램>>");
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("제품명\t수량\t판매단가\t매입단가\t운송료\t이익금");
+        System.out.println("-----------------------------------------------------------");
+    }
 }
