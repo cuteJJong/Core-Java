dbc71f70e83e1de01b686d8e6b1c2c54218a0795
cuteJJong <whdgh9588@gmail.com>
2024-07-22 17:01:57 +0900

    07월22일 자바소스

--- HospitalMgmt/src/Hospital.java
+++ HospitalMgmt/src/Hospital.java
@@ -1,3 +1,11 @@
+/*
+ * 작성자: 박종호
+ * 작성일: 2024-07-19
+ * 작성목적:병원관리프로그램 - Output
+ * 작성환경:windows 11 10.0 build 22631
+ *        JDK 17.0.11
+ *        intellij for Java Developers Version : 2024 -06
+ * */
 public class Hospital {
     private int number;
     private Department department;
